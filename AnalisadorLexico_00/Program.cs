d600a671df4359db9c786ea7dfe7cb84a196d566
Lekeu <lekeuffer@yahoo.com>
2023-08-22 08:36:04 -0300

    cpontinuação do código, ainda falta

--- AnalisadorLexico_00/Program.cs
+++ AnalisadorLexico_00/Program.cs
@@ -1,1 +1,16 @@
-﻿var i = 0;
+﻿using AnalisadorLexico_00;
+using AnalisadorLexico_00.Content;
+using Antlr4.Runtime;
+
+var fileName = "C:\\Users\\21070012\\Desktop\\Leka_VisualStudio\\AnalisadorLexico_00\\Content\\teste.ss";   //args[0]?
+var fileContents = File.ReadAllText(fileName);
+
+
+var inputStream = new AntlrInputStream(fileContents);
+var simpleLexer = new SimpleLexer(inputStream);
+var commonTokenStream = new CommonTokenStream(simpleLexer);
+var simpleParser = new SimpleParser(commonTokenStream);
+var simpleContext = simpleParser.program();
+var visitor = new SimpleVisitor();
+
+visitor.Visit(simpleContext);
--- AnalisadorLexico_00/SimpleVisitor.cs
+++ AnalisadorLexico_00/SimpleVisitor.cs
@@ -10,9 +10,19 @@ namespace AnalisadorLexico_00
 {
     internal class SimpleVisitor: SimpleBaseVisitor<object?>    // cada visitor vai retornar um objeto nullo
     {
+        private Dictionary<string, object> Variables { get; } = new();
         public override object VisitAssignment([NotNull] SimpleParser.AssignmentContext context)
         {
-            return base.VisitAssignment(context);
+            var varName = context.IDENTIFIER().GetText();
+            var value = Visit(context.expression());
+
+            Console.Write("varname --> " + varName);
+            Console.Write("varname --> " + varName.ToString());
+
+            Variables[varName] = value;
+
+            return null;
+            //return base.VisitAssignment(context);
         }
     }
 }
